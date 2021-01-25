294b43a37b09ba56822d9f11457ae26662efb866
chizer01 <75090507+chizer01@users.noreply.github.com>
2021-01-25 08:54:33 -0600

    Change project to enable easier AWS integration

---
+++ Controllers/Meetings.cs
@@ -0,0 +1,59 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Runtime;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LocalGovtReporterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Meetings : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetMeetingsAsync(string state, string jurisdiction, string county, string startDate, string endDate)
+        {
+            var credentials = new BasicAWSCredentials("accessKey", "secretKey");
+            var client = new AmazonDynamoDBClient(credentials, RegionEndpoint.USEast2);
+
+            Table table = Table.LoadTable(client, "Meeting");
+            ScanFilter scanFilter = new ScanFilter();
+
+            if (!string.IsNullOrEmpty(state)) 
+                scanFilter.AddCondition("State", ScanOperator.Equal, state);
+            if (!string.IsNullOrEmpty(jurisdiction))
+                scanFilter.AddCondition("Jurisdiction", ScanOperator.Equal, jurisdiction);
+            if (!string.IsNullOrEmpty(county))
+                scanFilter.AddCondition("County", ScanOperator.Equal, county);
+            if (!string.IsNullOrEmpty(startDate) && !string.IsNullOrEmpty(endDate))
+                scanFilter.AddCondition("MeetingDate", ScanOperator.Between, startDate, endDate);
+
+            ScanOperationConfig config = new ScanOperationConfig()
+            {
+                AttributesToGet = new List<string> { "MeetingID", "MeetingDate", "Jurisdiction", "State", "County", "MeetingType" },
+                Select = SelectValues.SpecificAttributes,
+                Filter = scanFilter
+            };
+
+            Search search = table.Scan(config);
+
+            var documents = await search.GetNextSetAsync();
+
+            return Ok(documents.ToArray().ToJsonPretty());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMeetingAsync(string id)
+        {
+            var credentials = new BasicAWSCredentials("accessKey", "secretKey");
+            var client = new AmazonDynamoDBClient(credentials, RegionEndpoint.USEast2);
+
+            Table table = Table.LoadTable(client, "Meeting");
+            Document document = await table.GetItemAsync(id);
+
+            return Ok(document.ToJsonPretty());
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LocalGovtReporterAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration Configuration { get; private set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            // global cors policy
+            app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true));
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
+                });
+            });
+        }
+    }
+}
